03d36dc509a46dc6e9211e524490609e5b66ce3c
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2022-08-30 17:47:08 +0430

    feat: Building the email-sender module

---
+++ src/utils/email-sender.ts
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv';
+import { createTransport } from 'nodemailer';
+
+dotenv.config();
+
+const transporter = createTransport({
+  service: 'gmail',
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+});
+
+const sendEmail = async (email: string, subject: string, message: string) => {
+  let emailInfo = await transporter.sendMail({
+    from: `Carino <${process.env.MAIL_USER}>`,
+    to: email,
+    subject: subject,
+    text: message,
+  });
+  return emailInfo;
+};
+
+export default sendEmail;
