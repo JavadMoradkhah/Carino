4fee9a61c6c30122e6eeed7127dfcbd589f7c3cf
Javad Moradkhah <javadmoradkhah.dev@gmail.com>
2022-08-31 16:25:28 +0430

    feat: Adding upload routes

--- src/startup/routes.ts
+++ src/startup/routes.ts
@@ -3,9 +3,11 @@ import brand from '../routes/brand';
 import auth from '../routes/auth';
 import cars from '../routes/cars';
 import carTypes from '../routes/car-types';
+import upload from '../routes/upload';
 import error from '../middleware/error';
 import cors from 'cors';
 import helmet from 'helmet';
+import path from 'path';
 import express, { Application } from 'express';
 
 export default function (app: Application) {
@@ -18,6 +20,9 @@ export default function (app: Application) {
   app.use('/api/auth', auth);
   app.use('/api/cars', cars);
   app.use('/api/carTypes', carTypes);
+  app.use('/api/upload', upload);
+
+  app.use('/uploads', express.static(path.resolve(process.cwd(), 'uploads')));
 
   app.use(error);
 }
